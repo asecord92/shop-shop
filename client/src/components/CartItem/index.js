01be5b9fd58544dd0df88fb44460a34b26da837c
Adam <ats92@cox.net>
2022-01-11 11:16:21 -0800

    added cart ui

---
+++ client/src/components/CartItem/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+
+const CartItem = ({ item }) => {
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = (item) => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+  };
+  const onChange = (e) => {
+    const value = e.target.value;
+
+    if (value === "0") {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+    }
+  };
+  return (
+    <div className="flexrow">
+      <div>
+        <img src={`/images/${item.image}`} alt="" />
+      </div>
+      <div>
+        {item.name}, ${item.price}
+      </div>
+      <div>
+        <span>Qty: </span>
+        <input
+          onChange={onChange}
+          type="number"
+          placeholder="1"
+          value={item.purchaseQuantity}
+        />
+        <span
+          role="img"
+          aria-label="trash"
+          onClick={() => removeFromCart(item)}
+        >
+          🗑️
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
